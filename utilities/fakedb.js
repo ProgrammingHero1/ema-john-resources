1e427da1f5bc6577b4515a3f8ec4bf64c148ba76
Programming Hero <53802153+ProgrammingHero1@users.noreply.github.com>
2022-03-17 15:05:04 -0500

    Add files via upload

---
+++ utilities/fakedb.js
@@ -0,0 +1,42 @@
+// use local storage to manage cart data
+const addToDb = id =>{
+    let shoppingCart = {};
+
+    //get the shopping cart from local storage
+    const storedCart = localStorage.getItem('shopping-cart');
+    if(storedCart){
+        shoppingCart = JSON.parse(storedCart);
+    }
+
+    // add quantity
+    const quantity = shoppingCart[id];
+    if(quantity){
+        const newQuantity = quantity + 1;
+        shoppingCart[id] = newQuantity;
+    }
+    else{
+        shoppingCart[id] = 1;
+    }
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
+}
+
+const removeFromDb = id =>{
+    const storedCart = localStorage.getItem('shopping-cart');
+    if(storedCart){
+        const shoppingCart = JSON.parse(storedCart);
+        if(id in shoppingCart){
+            delete shoppingCart[id];
+            localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
+        }
+    }
+}
+
+const deleteShoppingCart = () =>{
+    localStorage.removeItem('shopping-cart');
+}
+
+export {
+    addToDb, 
+    removeFromDb,
+    deleteShoppingCart
+}
